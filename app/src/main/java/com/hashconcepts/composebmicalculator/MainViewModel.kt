a0b47b4b1026075086daf2303e2c1239d22b9acc
ifechukwu <ifechukwu.udorji@accelerexholdings.com>
2022-05-31 11:15:56 +0100

    Initial commit

---
+++ app/src/main/java/com/hashconcepts/composebmicalculator/MainViewModel.kt
@@ -0,0 +1,11 @@
+package com.hashconcepts.composebmicalculator
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * @created 31/05/2022 - 11:14 AM
+ * @project ComposeBMICalculator
+ * @author  ifechukwu.udorji
+ */
+class MainViewModel : ViewModel() {
+}
---
+++ app/src/main/java/com/hashconcepts/composebmicalculator/ui/screens/detailscreen.kt
@@ -0,0 +1,7 @@
+package com.hashconcepts.composebmicalculator.ui.screens
+
+/**
+ * @created 31/05/2022 - 11:13 AM
+ * @project ComposeBMICalculator
+ * @author  ifechukwu.udorji
+ */
